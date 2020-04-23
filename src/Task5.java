0cdbc4319b294cc26b29e3346b69b0a8bd426a12
Evgenii.Mikhin <Evgenii.Mikhin@gmail.com>
2020-04-23 03:08:45 +0300

    home work №5

--- src/Task5.java
+++ src/Task5.java
@@ -1,5 +1,22 @@
 //Задача №5: Таблица умножения
 //Необходимо вывести на экран таблицу умножения от 1 до 10 для введённого пользователем любого числа. Использовать циклы запрещено.
-public class Task5 {
+import java.util.Scanner;
 
+public class Task5 {
+    public static void main(String args[]) {
+    Scanner in = new Scanner(System.in);
+        System.out.println("Введите число: ");
+    int a = in.nextInt();
+        System.out.println(a+"*1= "+a*1);
+        System.out.println(a+"*2= "+a*2);
+        System.out.println(a+"*3= "+a*3);
+        System.out.println(a+"*4= "+a*4);
+        System.out.println(a+"*5= "+a*5);
+        System.out.println(a+"*6= "+a*6);
+        System.out.println(a+"*7= "+a*7);
+        System.out.println(a+"*8= "+a*8);
+        System.out.println(a+"*9= "+a*9);
+
+    }
 }
+
