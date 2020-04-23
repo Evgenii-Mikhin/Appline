d28ea5ede4ab1e9f90a2f7692751ac7ba142fa1e
Evgenii.Mikhin <Evgenii.Mikhin@gmail.com>
2020-04-24 01:34:30 +0300

    home work №8

---
+++ src/Task8.java
@@ -0,0 +1,10 @@
+//Задача №8: Цикличный вывод данных на экран
+//Необходимо с помощью цикла вывести на экран числа от 1 до 50.
+public class Task8 {
+    public static void main(String args[]) {
+        for (int x = 1; x <= 50; x = x + 1) {
+            System.out.println(x);
+        }
+    }
+}
+
